142635038ab38fc9426fba4603250fad740b336f
Keith Williams <keith.dev.uk@gmail.com>
2018-04-13 11:42:05 +0100

    build with dlib 2a (implement pure virtual method)

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,17 @@
 #include <dlib/server.h>
 #include <iostream>
 
+class http_server : public dlib::server_http
+{
+public:
+	using dlib::server_http;
+
+private:
+	const string dlib::server_http::on_request(const dlib::incoming_things&, dlib::outgoing_things&) { return std::string(); }
+};
+
 int main()
 {
 	std::cout << "hello" << std::endl;
-	dlib::server_http();
+	http_server();
 }
